b7350812bdf57701828efcc0f61d2a2fea5f59eb
枫林残叶山 <1310831650@qq.com>
2021-01-27 16:58:37 +0800

    first commit

---
+++ module1/src/com/chen/git/Student.java
@@ -0,0 +1,22 @@
+package com.chen.git;
+
+public class Student {
+    private String stuName;
+    private Integer stuId;
+
+    public String getStuName() {
+        return stuName;
+    }
+
+    public void setStuName(String stuName) {
+        this.stuName = stuName;
+    }
+
+    public Integer getStuId() {
+        return stuId;
+    }
+
+    public void setStuId(Integer stuId) {
+        this.stuId = stuId;
+    }
+}
